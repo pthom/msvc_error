f598d9a707d3acb1d6d183225456028afade0e1d
Pascal Thomet <pthomet@gmail.com>
2022-10-18 11:41:19 +0200

    initial commit

---
+++ foo.cpp
@@ -0,0 +1,15 @@
+struct ImVec2
+{
+    float                                   x, y;
+    ImVec2(float _x, float _y)    : x(_x), y(_y) { }
+};
+
+
+bool blah2()
+{
+    auto f = [](const ImVec2 & size = ImVec2(0, 0)) -> bool
+    {
+        return true;
+    };
+    return f;
+}
